8737d2d23a4b9809ac51fe52316e232c4c422130
Luiz Fernando Gomes <48967131+lfernando-17@users.noreply.github.com>
2022-12-03 12:01:12 -0300

    Update TestTorneio.java

--- TORNEIO/src/tests/TestTorneio.java
+++ TORNEIO/src/tests/TestTorneio.java
@@ -1,5 +1,26 @@
 package tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import control.TorneioTenis;
+
 public class TestTorneio {
 
+	TorneioTenis tenis ;
+	public TestTorneio() {
+		tenis = new TorneioTenis();
+	}
+	
+	@Test
+	public void testVitoria() {
+		char[] Partidas = {'V','V','P','P','P','V'};
+		Assert.assertEquals(2,tenis.Torneio(Partidas),0);
+	}
+	@Test
+	public void testDerrota() {
+		char[] Partidas = {'P','P','P','P','P','P'};
+		Assert.assertEquals(-1,tenis.Torneio(Partidas),0);
+	}
+	
 }

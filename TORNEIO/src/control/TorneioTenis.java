b66e413928d6f8684f96d6bbdba6031bf164c575
Luiz Fernando Gomes <48967131+lfernando-17@users.noreply.github.com>
2022-12-03 12:00:51 -0300

    Update TorneioTenis.java

--- TORNEIO/src/control/TorneioTenis.java
+++ TORNEIO/src/control/TorneioTenis.java
@@ -4,28 +4,24 @@ import java.util.Scanner;
 import java.io.*;
 
 public class TorneioTenis {
-    public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
+    public int Torneio (char[] partidas) {
 	    
 	int wins = 0;
-	char resp;
 	
-	for (int i=0; i<6; i++) {
-	    resp = in.next().charAt(0);
-	    if(resp == 'V')
+	for (int i=0; i<partidas.length; i++) {
+	    if(partidas[i] == 'V')
 		wins++;
 	}
 	
 	if (wins == 0)
-	    System.out.println(-1);
+	    return(-1);
 	else if(wins <= 2)
-	    System.out.println(3);
+		return(3);
 	else if(wins <= 4)
-	    System.out.println(2);
+		return(2);
 	else
-	    System.out.println(1);
+		return(1);
 
 	}
 	    
  }
-
